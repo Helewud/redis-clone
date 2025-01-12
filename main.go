6440f0d0d7a5fbdd256ec784b60cc942e0d1736c
helewud <helewud@gmail.com>
2025-01-12 19:10:11 +0100

    chore: handle and read connections from client

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 func main() {
@@ -13,13 +15,37 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	defer l.Close()
 
-	// Listen for connections
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println(err)
-		return
+	// listen
+	for {
+		// Listen for connections
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		go func(conn net.Conn) {
+			defer conn.Close()
+
+			for {
+				buf := make([]byte, 1024)
+
+				// read message from client
+				_, err = conn.Read(buf)
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					fmt.Println("error reading from client: ", err.Error())
+					os.Exit(1)
+				}
+
+				// ignore request and send back a PONG
+				conn.Write([]byte("+OKK\r\n"))
+			}
+		}(conn)
 	}
 
-	defer conn.Close()
 }
